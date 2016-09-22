31b391075c92d9ab8e2e2e8a2c041b4f4836ff8d
Juan Carlos Medina <juanca.med@gmail.com>
2016-09-22 11:17:13 -0700

    Add output.publicPath
    Seems to break dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const StatsPlugin = require('stats-webpack-plugin');
 module.exports = {
   entry: './index.js',
 
+  output: {
+    publicPath: `//localhost:8080/`,
+  },
+
   plugins: [
     new StatsPlugin('manifest.json', {
       chunkModules: false,
