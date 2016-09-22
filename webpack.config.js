998adbf379843fbbbea6ba48e9a0e709fdf0a749
Juan Carlos Medina <juanca.med@gmail.com>
2016-09-22 10:51:22 -0700

    npm install --save stats-webpack-plugin && use it

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const StatsPlugin = require('stats-webpack-plugin');
+
 module.exports = {
   entry: './index.js',
+
+  plugins: [
+    new StatsPlugin('manifest.json', {
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true,
+    }),
+  ],
 };
